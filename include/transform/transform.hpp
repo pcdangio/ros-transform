735bc9e7b884fef577532e9c03c8eaf861aef50c
Paul D'Angio <pcdangio@gmail.com>
2021-02-24 19:41:40 -0500

    transform: initial class commit

---
+++ include/transform/transform.hpp
@@ -0,0 +1,73 @@
+/// \file transform/transform.hpp
+/// \brief Defines the transform::transform_t class.
+#ifndef TRANSFORM___TRANSFORM_H
+#define TRANSFORM___TRANSFORM_H
+
+#include <transform_msgs/transform.h>
+
+#include <eigen3/Eigen/Dense>
+
+/// \brief Utilities for creating and applying geometric transformations.
+namespace transform {
+
+/// \brief Represents a 3D transformation between coordinate frames.
+class transform_t
+{
+public:
+    // CONSTRUCTORS
+    /// \brief Instantiates a new identity transformation_t object.
+    transform_t();
+    /// \brief Instantiates a new transformation_t object.
+    /// \param translation The translation component of the transformation.
+    /// \param rotation The rotation component of the transformation.
+    transform_t(const Eigen::Vector3d& translation, const Eigen::Quaterniond& rotation);
+    /// \brief Instantiates a new transformation_t object with an identity rotation.
+    /// \param translation The translation component of the transformation.
+    transform_t(const Eigen::Vector3d& translation);
+    /// \brief Instantiates a new transformation_t object with an identity translation.
+    /// \param rotation The rotation component of the transformation.
+    transform_t(const Eigen::Quaterniond& rotation);
+    /// \brief Instantiates a new transformation_t object from a transform message.
+    /// \param transform_message The message to instantiate from.
+    transform_t(const transform_msgs::transform& transform_message);
+
+    // MODIFIERS
+    /// \brief Calculates the inverse of the transform.
+    /// \returns The inverted transform.
+    transform_t inverse() const;
+
+    // APPLICATIONS
+    /// \brief Performs an in-place chain on top of another transform.
+    /// \param transform The transform to chain in place.
+    void transform(transform_t& transform) const;
+    /// \brief Transforms a 3D point or vector in place.
+    /// \param vector The 3D point or vector to transform.
+    void transform(Eigen::Vector3d& vector) const;
+    /// \brief Transforms a 3D pose in place.
+    /// \param position The 3D position of the pose.
+    /// \param orientation The 3D euler angle representation of the pose orientation.
+    void transform(Eigen::Vector3d& position, Eigen::Vector3d& orientation);
+    /// \brief Transforms a 3D pose in place.
+    /// \param position The 3D position of the pose.
+    /// \param orientation The 3D quaternion representation of the pose orientation.
+    void transform(Eigen::Vector3d& position, Eigen::Quaterniond& orientation);
+
+    // ACCESS
+    /// \brief Gets the translation component of this transform.
+    /// \returns A const reference to the translation component.
+    const Eigen::Vector3d& translation() const;
+    /// \brief Gets the rotation component of this transform.
+    /// \returns A const reference to the rotation component.
+    const Eigen::Quaterniond& rotation() const;
+    
+private:
+    // VARIABLES
+    /// \brief The transform's translation component.
+    Eigen::Vector3d m_translation;
+    /// \brief The transform's rotation component.
+    Eigen::Quaterniond m_rotation;
+};
+
+}
+
+#endif
---
+++ src/transform/transform.cpp
@@ -0,0 +1,103 @@
+#include <transform/transform.hpp>
+
+using namespace transform;
+
+// CONSTRUCTORS
+transform_t::transform_t()
+{
+    transform_t::m_translation.setZero();
+    transform_t::m_rotation.setIdentity();
+}
+transform_t::transform_t(const Eigen::Vector3d& translation, const Eigen::Quaterniond& rotation)
+{
+    transform_t::m_translation = translation;
+    transform_t::m_rotation = rotation;
+}
+transform_t::transform_t(const Eigen::Vector3d& translation)
+{
+    transform_t::m_translation = translation;
+    transform_t::m_rotation.setIdentity();
+}
+transform_t::transform_t(const Eigen::Quaterniond& rotation)
+{
+    transform_t::m_translation.setZero();
+    transform_t::m_rotation = rotation;
+}
+transform_t::transform_t(const transform_msgs::transform& transform_message)
+{
+    transform_t::m_translation = {transform_message.x, transform_message.y, transform_message.z};
+    transform_t::m_rotation = {transform_message.qw, transform_message.qx, transform_message.qy, transform_message.qz};
+}
+
+// MODIFIERS
+transform_t transform_t::inverse() const
+{
+    // Create output transform.
+    // NOTE: This method allows Eigen to do operations in place.
+    transform_t inverted;
+
+    // Invert the translation.
+    inverted.m_translation = transform_t::m_translation * -1.0;
+
+    // Invert the rotation.
+    inverted.m_rotation = transform_t::m_rotation.inverse();
+
+    return inverted;
+}
+
+// APPLICATIONS
+void transform_t::transform(transform_t& transform) const
+{
+    // Apply this transform's rotation to the original transform.
+    transform.m_rotation = transform_t::m_rotation * transform.m_rotation;
+    transform.m_translation = transform_t::m_rotation * transform.m_translation;
+    // Normalize the transformed rotation for numerical stability.
+    transform.m_rotation.normalize();
+
+    // Add this transform's translation to the now rotated original translation.
+    transform.m_translation += transform_t::m_translation;
+}
+void transform_t::transform(Eigen::Vector3d& vector) const
+{
+    // Apply this transform's rotation to the original vector.
+    vector = transform_t::m_rotation * vector;
+
+    // Add the transform's translation to the rotated vector.
+    vector += transform_t::m_translation;
+}
+void transform_t::transform(Eigen::Vector3d& position, Eigen::Vector3d& orientation)
+{
+    // Convert the orientation to a quaternion.
+    Eigen::Quaterniond q = Eigen::AngleAxisd(orientation(0), Eigen::Vector3d::UnitX()) *
+                           Eigen::AngleAxisd(orientation(1), Eigen::Vector3d::UnitY()) *
+                           Eigen::AngleAxisd(orientation(2), Eigen::Vector3d::UnitZ());
+
+    // Transform the pose with it's quaternion form.
+    transform_t::transform(position, q);
+
+    // Convert the quaternion back to euler.
+    orientation = q.toRotationMatrix().eulerAngles(0, 1, 2);
+}
+void transform_t::transform(Eigen::Vector3d& position, Eigen::Quaterniond& orientation)
+{
+    // Apply this transform's rotation to the orientation.
+    orientation = transform_t::m_rotation * orientation;
+    // Normalize the transformed orientation for numerical stability.
+    orientation.normalize();
+
+    // Apply this transform's rotation to the position.
+    position = transform_t::m_rotation * position;
+
+    // Add this transform's translation to the position.
+    position += transform_t::m_translation;
+}
+
+// ACCESS
+const Eigen::Vector3d& transform_t::translation() const
+{
+    return transform_t::m_translation;
+}
+const Eigen::Quaterniond& transform_t::rotation() const
+{
+    return transform_t::m_rotation;
+}
